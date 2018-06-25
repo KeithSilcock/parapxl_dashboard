b72ae331dea87f0484e80e2f60593b9e562bb0fb
KeithSilcock <silcockk@gmail.com>
2018-06-25 13:39:52 -0700

    started separating components broke location and board components into smaller, easier to manage components added animation for boards to slide up removed un-used components added styling

--- client/src/components/DisplayComponents/TextBoard.js
+++ client/src/components/DisplayComponents/TextBoard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default props => {
   const { displayData } = props;
   return (
-    <div className="text-board container">
+    <div className="board-preview text-board container">
       <h2 className="text-board title">{displayData.title}</h2>
       <h4 className="text-board subtitle">{displayData.subtitle}</h4>
       <div className="text-board text-box">{displayData.content}</div>
